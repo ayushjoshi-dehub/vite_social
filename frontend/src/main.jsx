5171a27058f1ea05ff930e3200fa8fdfd12072ed
ayush-dehub <joshiayush78@gmail.com>
2026-06-12 15:12:22 +0545

    check

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,12 +7,10 @@ import { store } from './redux/store';
 import App from './App.jsx';
 import './index.css';
 
-const basename = import.meta.env.BASE_URL || '/';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter basename={basename}>
+      <HashRouter>
         <App />
       </HashRouter>
     </Provider>
