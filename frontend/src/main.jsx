42bef8c399d3a4cfc76ee915972baf804a354c45
ayush-dehub <joshiayush78@gmail.com>
2026-06-12 15:08:56 +0545

    check

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 // src/main.jsx  (or index.jsx)
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import App from './App.jsx';
@@ -12,9 +12,9 @@ const basename = import.meta.env.BASE_URL || '/';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename={basename}>
+      <HashRouter basename={basename}>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
